51568affdfe92107ccb7481842a69c385a1c85f7
opeyemijoseph <opsyyjoe@gmail.com>
2020-12-01 16:33:19 +0100

    Second Commit

--- inventory/settings.py
+++ inventory/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+import dj_database_url
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -132,3 +133,4 @@ AUTH_USER_MODEL = 'users.User'
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+django_heroku.settings(locals())
